dde077c02fc7270286f591644c05e08e0f16038e
Luca <luca24ever@gmail.com>
2025-03-23 17:41:51 +0100

    updates

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -3,7 +3,7 @@ from .average_drop import AverageDrop
 from .increase_in_confidence import IncreaseInConfidence
 from .deletion_curve import DeletionCurveAUC
 from .insertion_curve import InsertionCurveAUC
-from .infidelity import Infidelity
+from .infidelity import Infidelity, perturb_fn
 from .sensitivity import Sensitivity
 from .road import RoadCombined
 from .roc_auc import ROC_AUC
